7aa1387f225bad0789b72fe88afa1ed4876f5501
IvanOnohov <ivan.onokh.july@gmail.com>
2024-11-27 06:08:35 +0900

    Добавьте файлы проекта.

---
+++ Constat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = CalculateAbsoluteDifference(n);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static int CalculateAbsoluteDifference(int n)
+    {
+        int difference = Math.Abs(n - 123);
+        if (n > 123)
+        {
+            return 3 * difference; // Умножаем на 3, если n больше 123
+        }
+        return difference;
+    }
+}
